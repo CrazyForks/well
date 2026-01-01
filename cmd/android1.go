6ee9f6f4638933d8570a377c1458fbacae085d05
shynome <shynome@gmail.com>
2026-01-01 23:03:43 +0800

    不能使用其他包的类型

--- cmd/android1.go
+++ cmd/android1.go
@@ -2,7 +2,9 @@ package cmd
 
 import "remoon.net/well/wg"
 
-type MobileVPN = wg.MobileVPN
+type MobileVPN interface {
+	Start(token string) // 因为 POST /api/ipc/device 需要管理员权限, 所以传递下 token
+}
 
 func SetMobileVPN(vpn MobileVPN) {
 	wg.SetMobileVPN(vpn)
