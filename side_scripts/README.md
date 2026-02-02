590e66dea894beda84e8f592d38d5b9da2e83acf
shynome <shynome@gmail.com>
2026-02-02 23:38:22 +0800

    添加 allow_anyone 脚本

---
+++ side_scripts/README.md
@@ -0,0 +1,5 @@
+# 简介
+
+一些脚本帮助更好的使用 well-net, 通过 deno 运行
+
+- [allow_anyone.ts](./allow_anyone.ts) 允许所有人连接
---
+++ side_scripts/allow_anyone.ts
@@ -0,0 +1,69 @@
+import PocketBase, {
+  RecordAuthResponse,
+  RecordModel,
+} from "npm:pocketbase@0.26.8"
+
+const pb = new PocketBase("http://127.0.0.1:7799")
+pb.beforeSend = (url, options) => {
+  console.log("执行请求", options.method, url)
+  return { url, options }
+}
+
+const auth = {
+  user: "well@remoon.net",
+  pass: "well@remoon.net",
+}
+
+import { jwtDecode } from "npm:jwt-decode@4.0.0"
+// @ts-types="npm:@types/luxon@3.7.1"
+import { DateTime } from "npm:luxon@3.7.2"
+
+async function login(su?: RecordAuthResponse<RecordModel>) {
+  if (su) {
+    const token = jwtDecode(su.token)
+    const t = DateTime.fromSeconds(token.exp!)
+    const w = t.diffNow("seconds").seconds - 60
+    await new Promise((rl) => setTimeout(rl, w * 1e3))
+  }
+  return pb.collection("_superusers").authWithPassword(auth.user, auth.pass)
+}
+
+const su = await login()
+async function loginKeep(su: RecordAuthResponse<RecordModel>) {
+  while (true) {
+    su = await login(su)
+  }
+}
+loginKeep(su)
+
+interface Peer extends RecordModel {
+  disabled: boolean
+  ipv6: string
+}
+
+const peers = pb.collection<Peer>("peers")
+await peers.subscribe("*", (d) => {
+  if (d.action !== "create") {
+    return
+  }
+  if (!d.record.disabled) {
+    return
+  }
+  if (d.record.ipv6 != "") {
+    return
+  }
+  peers.update(d.record.id, { disabled: false, ipv6: "auto" })
+})
+
+async function fixLost() {
+  while (true) {
+    await new Promise((rl) => setTimeout(rl, 60e3))
+    const peerList = await peers.getFullList({
+      filter: pb.filter(`disabled = true && ipv6 = ''`),
+    })
+    for (const p of peerList) {
+      await peers.update(p.id, { disabled: false, ipv6: "auto" })
+    }
+  }
+}
+fixLost()
